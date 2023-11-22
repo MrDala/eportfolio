2c637c1dbfd666fea6d637a8e544db2e0621b56c
Alexis <LEON>
2023-11-22 17:35:43 +0100

    responsive volet exp pro

--- eportfolio/src/pages/Accueil.tsx
+++ eportfolio/src/pages/Accueil.tsx
@@ -65,15 +65,19 @@ function Accueil() {
             onClick: () => openLink('https://www.linkedin.com/in/alexis-leon1/')
           }}
         />
+
         <BlocTextImage name="Présentation" picture="bg">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
         </BlocTextImage>
+
         <BlocExpPro name="Expériences professionnelles">
           <CardExpPro picture="bg" poste="Nom du poste" entreprise="Nom de l'entreprise" adresse="adresse" isOpen={isVoletExpProOpen} toggleVoletExpPro={toggleVoletExpPro} date="Date du contrat">
           <h4>Sous-titre</h4>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Cursus eget nunc scelerisque viverra mauris. Laoreet sit amet cursus sit. Sem fringilla ut morbi tincidunt augue interdum velit. Facilisis leo vel fringilla est ullamcorper eget nulla. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Amet nulla facilisi morbi tempus iaculis urna id volutpat. Tellus integer feugiat scelerisque varius. Dictum varius duis at consectetur. Pellentesque habitant morbi tristique senectus et netus et. Libero id faucibus nisl tincidunt eget nullam. Id faucibus nisl tincidunt eget nullam. Rutrum tellus pellentesque eu tincidunt tortor aliquam nulla facilisi. Odio ut enim blandit volutpat maecenas volutpat. Tristique et egestas quis ipsum suspendisse. Sit amet consectetur adipiscing elit ut aliquam purus sit. Vel eros donec ac odio. Suspendisse interdum consectetur libero id faucibus nisl tincidunt eget nullam.</p>
           </CardExpPro>
         </BlocExpPro>
+
+        
       </div>
     </>
   );
