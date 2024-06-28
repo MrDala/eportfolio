9e5c2a968a548eb19a7fa5130196c5a817505593
Alexis LEON <alexis@MBP-de-Alexis.lan>
2024-06-28 23:31:03 +0200

    update content

--- App/src/index.tsx
+++ App/src/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from "react-router-dom";
-
 import { router } from './rooter/rooter';
 
 import "./style/Style.css";
